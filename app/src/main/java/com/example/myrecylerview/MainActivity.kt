61ea385bfed358350ea14235ebe25b17e2a4186b
alifahnurhana <alifahnur2609@gmail.com>
2022-01-26 18:51:52 +0800

    cardView

--- app/src/main/java/com/example/myrecylerview/MainActivity.kt
+++ app/src/main/java/com/example/myrecylerview/MainActivity.kt
@@ -9,22 +9,26 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var rvHeroes: RecyclerView
     private var list: ArrayList<Hero> = arrayListOf()
+    private var title: String = "Mode List"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setActionBarTitle(title)
 
-    rvHeroes = findViewById(R.id.rv_heroes)
+        rvHeroes = findViewById(R.id.rv_heroes)
         rvHeroes.setHasFixedSize(true)
 
         list.addAll(HeroesData.listData)
         showRecyclerList()
     }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-      menuInflater.inflate(R.menu.menu_main, menu)
-      return super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -32,25 +36,24 @@ class MainActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
-    private fun setMode(selectedMode: Int){
-       when (selectedMode){
-           R.id.action_list -> {
-               showRecyclerList()
+    private fun setMode(selectedMode: Int) {
+        when (selectedMode) {
+            R.id.action_list -> {
+                title = "Mode List"
+                showRecyclerList()
+            }
 
-           }
+            R.id.action_grid -> {
+                title = "Mode Grid"
+                showRecyclerGrid()
+            }
 
-           R.id.action_grid -> {
-               showRecycleGrid()
-
-           }
-
-           R.id.action_cardview ->{
-
-           }
-       }
-
-
-
+            R.id.action_cardview -> {
+                title = "Mode Grid"
+                showRecyclerCardView()
+            }
+        }
+        setActionBarTitle(title)
     }
 
     private fun showRecyclerList() {
@@ -59,9 +62,19 @@ class MainActivity : AppCompatActivity() {
         rvHeroes.adapter = listHeroAdapter
     }
 
-    private fun showRecycleGrid() {
+    private fun showRecyclerGrid() {
         rvHeroes.layoutManager = GridLayoutManager(this, 2)
         val gridHeroAdapter = GridHeroAdapter(list)
         rvHeroes.adapter = gridHeroAdapter
+    }
+
+    private fun showRecyclerCardView() {
+        rvHeroes.layoutManager = LinearLayoutManager(this)
+        val cardViewHeroAdapter = CardViewHeroAdapter(list)
+        rvHeroes.adapter = cardViewHeroAdapter
+    }
+
+    private fun setActionBarTitle(title: String) {
+        supportActionBar?.title = title
     }
 }
